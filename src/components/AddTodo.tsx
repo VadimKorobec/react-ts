15a5d95c1102cea02ed7e24c7015aa4bc7bac10c
VadimKorobec <_s_e_t__@ukr.net>
2023-06-19 17:39:32 +0300

    Update AddTodo.tsx

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -3,9 +3,14 @@ import { useRef } from "react";
 
 export const AddTodo: React.FC = () => {
   const titleInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Add title
           <input type="text" id="add-todo" ref={titleInputRef} />
