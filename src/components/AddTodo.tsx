517ecb32704b7d816f50be14db96d74ca018fbeb
VadimKorobec <_s_e_t__@ukr.net>
2023-06-19 17:34:37 +0300

    Update AddTodo.tsx

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useRef } from "react";
 
 export const AddTodo: React.FC = () => {
+  const titleInputRef = useRef<HTMLInputElement>();
   return (
     <div>
       <form>
         <label>
           Add title
-          <input type="text" />
+          <input type="text" id="add-todo" />
         </label>
         <button type="submit">Add Todo</button>
       </form>
