45fcf9ef956dcec5bf55396d89fe0187e5ed99c5
VadimKorobec <_s_e_t__@ukr.net>
2023-06-19 17:09:49 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  const todos = [{ id: "1", title: "text" }];
+  return (
+    <div className="App">
+      <TodoList todos={todos} />
+    </div>
+  );
 };
 
 export default App;
