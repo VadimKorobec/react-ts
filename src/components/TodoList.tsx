86ad692065ea778aeb2bf9b3e5dd5d17086688c4
VadimKorobec <_s_e_t__@ukr.net>
2023-06-19 17:14:00 +0300

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -13,11 +13,8 @@ interface IState {
   count: number;
 }
 
-class TodoList extends React.Component<IProps, IState> {
-  stateHandler() {}
-  render(): React.ReactNode {
-    return null;
-  }
-}
+const TodoList: React.FC = () => {
+  return <div></div>;
+};
 
 export default TodoList;
