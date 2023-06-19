c14aa3892fb6ed549e7549a714c850bec56d2a4f
VadimKorobec <_s_e_t__@ukr.net>
2023-06-19 18:35:09 +0300

    Update AddTodo.tsx

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { useRef } from "react";
+import React, { useState } from "react";
+import { IItem } from "../types/todo";
 
-export const AddTodo: React.FC = () => {
-  const titleInputRef = useRef<HTMLInputElement>(null);
+interface IProps {
+  onAddTodo: (todo: IItem) => void;
+}
 
-  const handleSubmit = (event) => {
+type OnlyTitle = Pick<IItem, "title">;
+
+export const AddTodo: React.FC<IProps> = (props) => {
+  const [todo, titleTodo] = useState<Partial<OnlyTitle>>({});
+
+  const titleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    titleTodo({
+      title: e.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+    if (!todo.title) {
+      return;
+    }
+
+    props.onAddTodo(todo as IItem);
   };
 
   return (
@@ -13,7 +30,7 @@ export const AddTodo: React.FC = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Add title
-          <input type="text" id="add-todo" ref={titleInputRef} />
+          <input type="text" id="add-todo" onChange={titleHandler} />
         </label>
         <button type="submit">Add Todo</button>
       </form>
